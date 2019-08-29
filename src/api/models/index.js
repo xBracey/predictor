e3705724238f8d3626b02d931fb2f9de6b8bcd31
Thomas Brace <tom@Thomass-MacBook-Air.local>
2019-08-29 23:39:49 +0100

    Revert loads of shit

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,45 +1,33 @@
 import Sequelize from "sequelize";
-import user from "./user";
-import group from "./group";
-import team from "./team";
-import group_match from "./group_match";
-import league from "./league";
-import user_league from "./user_league";
-import player from "./player";
-import knockout_match from "./knockout_match";
-import group_match_scorer from "./group_match_scorer";
-import knockout_match_scorer from "./knockout_match_scorer";
-import group_prediction from "./group_prediction";
-import knockout_prediction from "./knockout_prediction";
 
-const sequelize =
-  process.env.NODE_ENV === "development"
-    ? new Sequelize(
-        process.env.DATABASE,
-        process.env.DATABASE_USER,
-        process.env.DATABASE_PASSWORD,
-        {
-          dialect: "postgres"
-        }
-      )
-    : new Sequelize(process.env.DATABASE_URL, {
+const sequelize = process.env.DATABASE_HOST
+  ? new Sequelize(
+      process.env.DATABASE,
+      process.env.DATABASE_USER,
+      process.env.DATABASE_PASSWORD,
+      {
         dialect: "postgres",
-        protocol: "postgres"
-      });
+        host: process.env.DATABASE_HOST
+      }
+    )
+  : new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres"
+    });
 
 const models = {
-  Group: group,
-  Player: player,
-  Team: team,
-  League: league,
-  Group_Match: group_match,
-  Knockout_Match: knockout_match,
-  User: user,
-  Group_Match_Scorer: group_match_scorer,
-  Knockout_Match_Scorer: knockout_match_scorer,
-  Group_Prediction: group_prediction,
-  Knockout_Prediction: knockout_prediction,
-  User_League: user_league
+  Group: sequelize.import("./group"),
+  Player: sequelize.import("./player"),
+  Team: sequelize.import("./team"),
+  League: sequelize.import("./league"),
+  Group_Match: sequelize.import("./group_match"),
+  Knockout_Match: sequelize.import("./knockout_match"),
+  User: sequelize.import("./user"),
+  Group_Match_Scorer: sequelize.import("./group_match_scorer"),
+  Knockout_Match_Scorer: sequelize.import("./knockout_match_scorer"),
+  Group_Prediction: sequelize.import("./group_prediction"),
+  Knockout_Prediction: sequelize.import("./knockout_prediction"),
+  User_League: sequelize.import("./user_league")
 };
 
 Object.keys(models).forEach(key => {
