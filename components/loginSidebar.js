f6ee646e2266fc056a0b0808dce40988210ed495
Thomas Brace <tom@Thomass-MacBook-Air.local>
2019-09-07 16:22:11 +0100

    Start homepage

---
+++ components/loginSidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+class LoginSidebar extends React.Component {
+  renderLogo() {
+    return <img className="login-sidebar__logo" src="static/footyBee.svg" />;
+  }
+  render() {
+    return <div className="login-sidebar">{this.renderLogo()}</div>;
+  }
+}
+
+export default LoginSidebar;
