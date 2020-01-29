712e090a3d349904177a3396c87d02fa0324fac6
Thomas Brace <thomas.brace@komododigital.co.uk>
2020-01-29 18:56:08 +0000

    Add james as a user

--- src/seeds/user.js
+++ src/seeds/user.js
@@ -16,6 +16,14 @@ const users = [
     name: "Thomas Brace",
     verified: true,
     admin: true
+  },
+  {
+    username: "JBrace",
+    password: "$2b$10$KM8VTwT5uuQe.GCHs1P69.S4L304uxJOo8ODG90fbDETM4IgB5vAO",
+    email: "james.h.brace@gmail.com",
+    name: "James Brace",
+    verified: true,
+    admin: true
   }
 ];
 
